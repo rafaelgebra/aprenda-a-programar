8573894bbbd8a8edc2763eea41819b9883bccd63
rafaelgebra <rafaelrodriguesgebra@gmail.com>
2022-08-26 14:38:23 -0300

    Teste5

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,12 @@
-let = nome = "Rafael Rodrigues Gebra"
-let = email = "rafaelrodriguesgebra@gmail.com"
-let = professor = "Thales"
-let = Idade = "34"
-let = Viagens = "O mundo"
+let nome = "Rafael Rodrigues Gebra"
+let email = "rafaelrodriguesgebra@gmail.com"
+let professor = "Thales"
+let idade = "34"
+let viagens = "O mundo"
+console.log (nome)  
+console.log (email)
+console.log (professor)
+console.log (idade)
+console.log (viagens)
+
+console.log (nome, email, professor, idade, viagens) 
